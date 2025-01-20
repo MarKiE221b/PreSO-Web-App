1f234bb1564ba5a10e0ced576966fa21512ccd8b
Marc Espiritu <mespiritu83795@liceo.edu.ph>
2025-01-20 16:34:46 +0800

    * Added logo for footer, * Added links to button

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import logo from "../assets/preSo_logo.png";
+import chedLogo from "../assets/CHED-LOGO_orig (1).png";
+import { BsCloudUpload } from "react-icons/bs";
+
 import ThemeController from "../components/ThemeController";
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 
 const MainLayout = () => {
   return (
@@ -10,7 +13,7 @@ const MainLayout = () => {
       <div className="navbar bg-base-100 px-4">
         <div className="flex-1">
           <div className="text-xl">
-            <img src={logo} className="h-14 w-14" alt="logo"/>
+            <img src={logo} className="h-14 w-14" alt="logo" />
           </div>
         </div>
         <div className="flex-none gap-2">
@@ -49,7 +52,7 @@ const MainLayout = () => {
                 <a>Settings</a>
               </li>
               <li>
-                <a>Logout</a>
+                <Link to="/">Logout</Link>
               </li>
             </ul>
           </div>
@@ -60,7 +63,7 @@ const MainLayout = () => {
       <div className="mx-4 mt-4">
         <ul className="menu bg-base-200 lg:menu-horizontal rounded-box">
           <li>
-            <a>
+            <Link to="/admin/dashboard">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -77,33 +80,14 @@ const MainLayout = () => {
               </svg>
               Dashboard
               <span className="badge badge-sm">99+</span>
-            </a>
+            </Link>
           </li>
           <li>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              Updates
+            <Link to="/admin/submission">
+              <BsCloudUpload size="20px" />
+              Submissions
               <span className="badge badge-sm badge-warning">NEW</span>
-            </a>
-          </li>
-          <li>
-            <a>
-              Stats
-              <span className="badge badge-xs badge-info"></span>
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
@@ -117,17 +101,8 @@ const MainLayout = () => {
 
       <footer className="footer bg-base-300 items-center p-4 mt-auto">
         <aside className="grid-flow-col items-center">
-          <svg
-            width="36"
-            height="36"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            fillRule="evenodd"
-            clipRule="evenodd"
-            className="fill-current"
-          >
-            <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-          </svg>
+          <img src={chedLogo} alt="ched-logo" className="h-[25px]" />
+
           <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
         </aside>
       </footer>
